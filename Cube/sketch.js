9702e0019400277ab757bb0c16059270db33647d
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-26 14:58:48 +0000

    Commiting here because the z axis is not working and I want to try some things

--- Cube/sketch.js
+++ Cube/sketch.js
@@ -6,23 +6,32 @@ function setup() {
 }
 
 function draw() {
-  background(175);
+  background(100);
+  ortho();
+
+
+  translate(0, 50, -50);
+  rotateX( -PI / 4);
  // translate(width / 2, height / 2);
   rectMode(CENTER);
 
+  //rotateX(angle * 0.25);
+
+
   let offset = 0;
-  for(let x = 0; x < width; x+= w) {
+  for(let y = 0; y < height; y+= w) {
+    for(let x = 0; x < width; x+= w) {
     push();
     let a = angle + offset;
     let h = map(sin(a), -1, 1, 0, 100);
-    fill(255);
-    translate(x - width /2 , 0, 0);
-    box(w);
+    normalMaterial();
+    translate(x - width /2, y - height/2, 0);
+    box(w - 2, h, w -2);
 //    rect(x - width/2 + w/2, 0, w - 2, h);
     offset += 0.1;
     pop();
-}
-
+    }
+  }
   angle += 0.1;
 }
 
