e0b2cb5db8fdd2971098bb94851360d5d84d45a5
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-26 14:41:28 +0000

    Moving from 2d to 3d with WEBGL

--- Cube/sketch.js
+++ Cube/sketch.js
@@ -1,23 +1,28 @@
 let angle = 0;
 let w = 24;
+
 function setup() {
   createCanvas(400, 400, WEBGL);
-  console.log('setup function section');
 }
 
 function draw() {
-  background(0);
+  background(175);
  // translate(width / 2, height / 2);
   rectMode(CENTER);
 
   let offset = 0;
   for(let x = 0; x < width; x+= w) {
+    push();
     let a = angle + offset;
     let h = map(sin(a), -1, 1, 0, 100);
     fill(255);
-    rect(x - width/2 + w/2, 0, w - 2, h);
+    translate(x - width /2 , 0, 0);
+    box(w);
+//    rect(x - width/2 + w/2, 0, w - 2, h);
     offset += 0.1;
+    pop();
 }
 
   angle += 0.1;
 }
+
