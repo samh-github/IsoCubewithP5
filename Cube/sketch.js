16841bc1fe16db79e24f2a95bed1885cfebff696
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-26 13:50:29 +0000

    Applying Oscillation and moving to WEBGL

--- Cube/sketch.js
+++ Cube/sketch.js
@@ -1,24 +1,23 @@
 let angle = 0;
-
-
-
-
+let w = 24;
 function setup() {
-
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
   console.log('setup function section');
-
 }
 
 function draw() {
-  // put drawing code here
   background(0);
-  translate(width / 2, height / 2);
+ // translate(width / 2, height / 2);
   rectMode(CENTER);
 
-  let h = 100;
-  fill(255);
-  rect(0,0, 10, h);
+  let offset = 0;
+  for(let x = 0; x < width; x+= w) {
+    let a = angle + offset;
+    let h = map(sin(a), -1, 1, 0, 100);
+    fill(255);
+    rect(x - width/2 + w/2, 0, w - 2, h);
+    offset += 0.1;
+}
 
   angle += 0.1;
 }
