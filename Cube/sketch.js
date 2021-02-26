9587ecb644fc2af6a71a0f3527844999e91cc226
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-26 15:41:08 +0000

    Final result!

--- Cube/sketch.js
+++ Cube/sketch.js
@@ -1,37 +1,35 @@
 let angle = 0;
-let w = 24;
+let w = 30;
+let ma;
+let maxD;
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(500, 500, WEBGL);
+  ma = atan(1/ sqrt(2));
+  maxD = dist(0, 0, 200, 200);
 }
 
 function draw() {
-  background(100);
-  ortho();
+  background(37, 3, 51);
+  ortho(-500, 500, -500, 500, 0, 1000);
+  rotateX(-ma);
+  rotateY(-QUARTER_PI);
 
 
-  translate(0, 50, -50);
-  rotateX( -PI / 4);
- // translate(width / 2, height / 2);
-  rectMode(CENTER);
-
-  //rotateX(angle * 0.25);
-
-
-  let offset = 0;
-  for(let y = 0; y < height; y+= w) {
+  for(let z = 0; z < height; z += w) {
     for(let x = 0; x < width; x+= w) {
     push();
+    let d = dist(x, z, width / 2, height / 2);
+    let offset = map(d, 0, maxD, -PI, PI);
     let a = angle + offset;
-    let h = map(sin(a), -1, 1, 0, 100);
+    let h = floor(map(sin(a), -1, 1, 60, 240));
+    translate(x - width / 2, 0, z - height / 2 );
     normalMaterial();
-    translate(x - width /2, y - height/2, 0);
     box(w - 2, h, w -2);
 //    rect(x - width/2 + w/2, 0, w - 2, h);
-    offset += 0.1;
     pop();
     }
   }
-  angle += 0.1;
+  angle -= 0.05;
 }
 
